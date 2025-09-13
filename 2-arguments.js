b573e66d2b1899b84647f3d79b52e8f1e60251a8
mohamed elsayed <melsayed20009@gmail.com>
2025-09-13 16:53:55 +0300

    2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,7 @@
+const arg = process.argv.length -2;
+if (arg=== 0)
+    {console.log("no argument")}
+else if (arg===1)
+    {console.log(" argument found")}
+else 
+    {console.log(" argument found")};
