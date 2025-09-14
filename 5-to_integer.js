f095aacadcf30d72c1e964b7797329a38a0f0c1b
Melsayed2025 <Melsayed20009@gmail.com>
2025-09-14 12:18:47 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,3 +1,8 @@
-let arg = process.argv[2]
-  let nums = parseInt(arg)
-  if(  isNaN (nums)){ console.log("Not a number") } else {console.log("my number: " + nums)}
+let arg = process.argv[2];
+let nums = parseInt(arg);
+
+if (isNaN(nums)) {
+  console.log("Not a number");
+} else {
+  console.log("My number: " + nums);
+}
